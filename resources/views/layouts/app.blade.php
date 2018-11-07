5efceadbeedead9a09cce20a01ea0377bdb33f24
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2018-11-07 07:22:48 +0100

    Intégration de la page d'accueil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,72 +8,49 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Bewallet') }} @yield('title')</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('lib/bootstrap4/css/bootstrap.min.css') }}" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('stylesheets')
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+    <header>
+        <nav role="navigation" class="navbar navbar-expand-xl navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand logo" href="#"><b>B<span>WALLET</span></b></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link logo" href="#">HOME<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">TRANSFER MONEY</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">DEPOSIT</a>
+                            <a class="dropdown-item" href="#">TRANSFER MONEY</a>
+                            <a class="dropdown-item" href="#">PAY A BWALLET FRIEND</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PAY ONLINE</a>
+                    </li>
+                </ul>
+                <a href="{{ route('login') }}" class="btn btn-outline-warning my-2 my-sm-0" type="submit">Login</a>
+                <a href="{{ route('register') }}" class="btn btn-outline-success my-2 my-sm-0" type="submit">Register</a>
             </div>
         </nav>
+    </header>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    @yield('content')
+
+    <script src="{{ asset('lib/jquery-slim.min.js') }}"></script>
+    <script src="{{ asset('lib/popper.min.js') }}"></script>
+    <script src="{{ asset('lib/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('lib/holder.min.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,142 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('title', ' - Home')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('stylesheets')
+    <link href="{{ asset('css/carousel.css') }}" rel="stylesheet">
+@endsection
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+@section('content')
+    <main role="main">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="first-slide" src="{{ asset('img/carousel/money-transfer-.jpg') }}" alt="First slide">
+                    <div class="container">
+                        <div class="carousel-caption text-left">
+                            <h1>TRANSFER MONEY</h1>
+                            <h2>FOR FREE</h2>
+                            <p><h5>No transfer fee when you send directly to a bank account.
+                                No hidden fee. No fees at all. Yes, really.</h5></p>
+                            <p><a class="btn btn-lg btn-success" href="{{ route('register') }}" role="button">Sign up today</a></p>
+                        </div>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="carousel-item">
+                    <img class="second-slide" src="{{ asset('img/carousel/img2.png') }}" alt="Second slide">
+                    <div class="container">
+                        <div class="carousel-caption text-left">
+                            <h1>ONLINE PAYMENT.</h1>
+                            <p></p>
+                            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-    </body>
-</html>
+
+
+        <!-- Marketing messaging and featurettes
+        ================================================== -->
+        <!-- Wrap the rest of the page in another container to center all the content. -->
+
+        <div class="container marketing">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/confidence.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>INSTANT</h2>
+                    <p>It’s easy for Skrill wallet holders to send and receive money – you just need an email address. And with fees of only 1%, more money arrives.</p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/convinence.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>CONFIDENCE</h2>
+                    <p>Your security is a priority. We always keep your payments and personal information safe, and our anti-fraud team protects every transaction.</p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/secure.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>CONVENIENCE</h2>
+                    <p>Send and receive money, store cards, link bank accounts and pay conveniently anytime and anywhere with your email address and password.</p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+            <hr class="featurette-divider">
+            <!-- Three columns of text below the carousel -->
+            <div class="text-center"><h1>HOW DOES IT WORK ?</h1></div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/user.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>CREATE AN ACCOUNT</h2>
+                    <p>It takes just a few minutes to create a BWallet wallet and bring all your payment details together in one place. You can choose from 40 currencies.</p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/make_payment.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>MAKE PAYMENT</h2>
+                    <p>Stay in control with simple, secure and quick payment methods, including stored cards and your account balance.</p>
+                </div><!-- /.col-lg-4 -->
+                <div class="col-lg-4">
+                    <img class="rounded-circle" src="{{ asset('img/carousel/withdraws.png') }}" alt="Generic placeholder image" width="200" height="200">
+                    <h2>WITHDRAW FUNDS</h2>
+                    <p>Move funds – including winnings – from Skrill to your bank account instantly or make ATM withdrawals.</p>
+                </div><!-- /.col-lg-4 -->
+            </div><!-- /.row -->
+
+
+            <!-- START THE FEATURETTES -->
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">EXCLUSIVE <span class="text-muted">BWALLET OFFERS</span></h2>
+                    <p class="lead">Enjoy easy access to a range of the latest merchant offers, deals and bonuses. Updated every month.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" src="{{ asset('img/carousel/online_payment.jpg') }}" alt="Generic placeholder image">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Access your money <span class="text-muted">wherever</span> you are 24/7</h2>
+                    <p class="lead">Our fast, easy to use and secure app lets you access your account wherever you need it.</p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img class="featurette-image img-fluid mx-auto" src="{{ asset('img/carousel/disponible.png') }}" alt="Generic placeholder image">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- /END THE FEATURETTES -->
+
+        </div><!-- /.container -->
+
+        <!-- FOOTER -->
+        <footer class="container ">
+            <p class="float-left"><a href="#">ABOUT US </a>|</p><p class="float-left"><a href="#"> SUPPORT </a>|</p><p class="float-left"><a href="#"> SECURITY </a>|</p><p class="float-left"><a href="#"> FEES </a>|</p>
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2018 BWallet Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </main>
+@endsection
+
+@section('scripts')
+
+@endsection
