67c3e2cc426b6dcdd4483822754b912f8b4a5c6b
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2018-11-22 10:35:28 +0100

    Retrait du background sur l'interface de connexion et d'inscription

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -98,9 +98,9 @@
     <script src="{{ asset('lib/bootstrap/js/bootstrap.min.js') }}"></script>
     <!--BACKSTRETCH-->
     <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
-    <script type="text/javascript" src="{{ asset('lib/jquery.backstretch.min.js') }}"></script>
+    <!--<script type="text/javascript" src="{{ asset('lib/jquery.backstretch.min.js') }}"></script>-->
     <script>
-        $.backstretch("img/login-bg.jpg", { speed: 500 });
+        //$.backstretch("img/login-bg.jpg", { speed: 500 });
     </script>
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -97,9 +97,9 @@
     <script src="{{ asset('lib/bootstrap/js/bootstrap.min.js') }}"></script>
     <!--BACKSTRETCH-->
     <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
-    <script type="text/javascript" src="{{ asset('lib/jquery.backstretch.min.js') }}"></script>
+    <!--<script type="text/javascript" src="{{ asset('lib/jquery.backstretch.min.js') }}"></script>-->
     <script>
-        $.backstretch("img/login-bg.jpg", { speed: 500 });
+        // $.backstretch("img/login-bg.jpg", { speed: 500 });
         $(function(){
             const authSuccess = $('#login-page').data('success');
             if(authSuccess === 1) {
